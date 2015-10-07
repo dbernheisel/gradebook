a6170eedd1b30fd0b19884d1a335dda905e1a8a7
David Bernheisel <david@bernheisel.com>
2015-10-06 21:48:29 -0400

    Add session control

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.destroy_all
+
+97.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(10, 20),
+    role: ["instructor", "student", "guardian"].sample,
+    )
+end
+
+User.create!(
+  email: "instructor@test.com",
+  password: "test1234",
+  role: "instructor")
+
+User.create!(
+  email: "student@test.com",
+  password: "test1234",
+  role: "student")
+
+User.create!(
+  email: "guardian@test.com",
+  password: "test1234",
+  role: "guardian")
+
+100.times do
+  GuardianRelationship.create!(
+    guardian_id: (1..25).to_a.sample,
+    student_id: (26..50).to_a.sample
+    )
+  InstructorRelationship.create!(
+    instructor_id: (51..100).to_a.sample,
+    student_id: (26..50).to_a.sample
+    )
+end
